ca336481e4d7949494b9900f3f4bdaa5bdfe3b53
mariam noorani <mariamnoorani00@gmail.com>
2026-04-30 18:10:17 +0530

    Update import from langchain_core

--- rag_engine.py
+++ rag_engine.py
@@ -5,7 +5,7 @@ from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
 from langchain.chains import RetrievalQA
-from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate
 
 
 class RAGEngine:
