6296a5d8fe7a0239ced7bcfdb4ca1578cd92fda6
mariam noorani <mariamnoorani00@gmail.com>
2026-05-02 17:12:05 +0530

    Clear contents of simple_app.py
    Remove all content from simple_app.py.

--- simple_app.py
+++ simple_app.py
@@ -1,12 +1,1 @@
-import streamlit as st
 
-st.title("📚 Personal Research Assistant")
-st.write("Testing if basic UI works")
-
-uploaded_file = st.file_uploader("Upload PDF", type=['pdf'])
-
-if uploaded_file:
-    st.success("File uploaded!")
-    
-if st.button("Test Button"):
-    st.write("Button works!")
